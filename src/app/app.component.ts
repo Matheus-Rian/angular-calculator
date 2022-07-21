77b32e861ea5ccbb47cf243b2bb3b03c1ac20570
Matheus Rian <souzamatheusrian@gmail.com>
2022-07-21 12:29:14 -0300

    refactor: removed excess responsibilities of app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,30 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent {
-  numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  operationsValue = ['/', 'x', '-', '+', '='];
-  currentValue = '';
-  displayValueInput = '0';
-
-  handleAddValuesInput(value: string) {
-    if (value === '=')
-      return this.result();
-
-    this.currentValue += value;
-    this.displayValueInput = this.currentValue;
-  }
-
-  result() {
-    if (this.currentValue)
-      this.displayValueInput = eval(this.currentValue);
-  }
-
-  resetValue() {
-    this.currentValue = '';
-    this.displayValueInput = '0';
-  }
+  constructor() { }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { GithubService } from 'src/services/github.service';
 import { AppComponent } from './app.component';
+import { CalculatorComponent } from './components/calculator/calculator.component';
+import { InfoGithubComponent } from './components/info-github/info-github.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorComponent,
+    InfoGithubComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    GithubService,
+  ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
